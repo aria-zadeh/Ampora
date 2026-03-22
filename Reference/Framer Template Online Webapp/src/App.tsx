10b37a9e0f9b6038e250afbab6baf0675e1dca8c
bob the builder <aria.nabavi09@gmail.com>
2026-03-22 18:03:39 -0400

    reference

---
+++ Reference/Framer Template Online Webapp/src/App.tsx
@@ -0,0 +1,93 @@
+import './framer/styles.css'
+
+import VerticalVideoFrameFramerComponent from './framer/vertical-video-frame'
+import MentorCardFramerComponent from './framer/mentor-card'
+import QualityCardFramerComponent from './framer/quality-card'
+import CourseCardFramerComponent from './framer/course-card'
+import PriceCardFramerComponent from './framer/price-card'
+import TestimonialCardFramerComponent from './framer/testimonial-card'
+import VideoPlayerHeroFrameFramerComponent from './framer/video-player-hero-frame'
+import TagMFramerComponent from './framer/tag-m'
+import PrimaryButtonFramerComponent from './framer/primary-button'
+import SocialProofFramerComponent from './framer/social-proof'
+import TagLFramerComponent from './framer/tag-l'
+import CurriculamAccordionFramerComponent from './framer/curriculam-accordion'
+import StatisticsFramerComponent from './framer/statistics'
+
+export default function App() {
+  return (
+    <div className='flex flex-col items-center gap-3 bg-[white]'>
+      <VerticalVideoFrameFramerComponent.Responsive
+        EAEiSauhj={"Upload"}
+        MsvJwK1BJ={"Esther Howard"}
+        TLJESPcm3={"Robert Fox"}
+        WsQwzFBaF={"John Jacob"}
+        e88rz9lnq={"Cameron Williamson"}
+        fF6UI6Edm={"Upload"}
+        jaQPopA3I={"Upload"}
+        yeljXuNeI={"Upload"}
+      />
+      <MentorCardFramerComponent.Responsive
+        BxTEWeXzC={"Award-winning editor with over a decade of experience crafting commercials, documentaries, and social campaigns. He’s collaborated with top-tier brands and creative teams worldwide, and now he’s sharing every practical skill, workflow, and creative insight he’s gained—without holding anything back."}
+        KC0kxLqKZ={"Senior Video Editor at Zenith"}
+        NIeT2WXix={"Danial Cruze"}
+        WiAqS5pPk={"https://www.instagram.com/"}
+        YrwevER2x={"www.x.com"}
+        f6p19kOhb={"https://www.linkedin.com/"}
+      />
+      <QualityCardFramerComponent.Responsive
+        ahDfLaTNf={"Industry-Level Training"}
+        sN7BLrwsH={"Master the key techniques used by professional editors everywhere."}
+      />
+      <CourseCardFramerComponent.Responsive
+        kQdMswT5S={false}
+        xROG3ExIr={"/#pricing"}
+      />
+      <PriceCardFramerComponent.Responsive
+        AWI2viap7={"/contact"}
+        dmF22hZf1={false}
+      />
+      <TestimonialCardFramerComponent.Responsive
+        I4rBN3uqD={5}
+        Rz8DrLSUz={"\"The courses are so well-structured, the whole learning platform is fantastic!\""}
+        eR9ZmkLGf={"Cooper Geidt"}
+      />
+      <VideoPlayerHeroFrameFramerComponent.Responsive
+        enyecMhXi={"https://youtu.be/8AHPXm9Y6mI"}
+        gqRz5THmT={"hvlWPyP3i"}
+        sKRpUCW6c={"Upload"}
+      />
+      <TagMFramerComponent.Responsive
+        iiNQ6sd0D={"Why Chose Us?"}
+      />
+      <PrimaryButtonFramerComponent.Responsive
+        N0JqvxlHe={false}
+        QmLq7qL0a={"/#pricing"}
+        i8ZiYu6Ux={"Enroll Now for $299"}
+      />
+      <SocialProofFramerComponent.Responsive
+        ETJt__o2i={"Trusted by 1000+Creators"}
+      />
+      <TagLFramerComponent.Responsive/>
+      <CurriculamAccordionFramerComponent.Responsive
+        DbQxRKlkt={"Week 1"}
+        Q4x212Mf5={"5. Importing media & creating organized project folders"}
+        Vqq1FMppq={"6. Building your first timeline and placing your first clips"}
+        ZsES6Uhz8={"3. Installing & setting up Premiere Pro / DaVinci Resolve"}
+        h83KHwhCX={"Module 1"}
+        n2idrqpIe={"Editing Basics & Software Mastery"}
+        nO_9WwLPr={"1. Introduction to video editing terminology & workflow"}
+        tzTzT7r5S={"2. Understanding resolution, frame rate, aspect ratios"}
+        ySVnZYYwD={"4.  Exploring the interface: panels, tools, shortcuts"}
+        zOXNBD662={"7. Saving projects & understanding autosave/backup"}
+      />
+      <StatisticsFramerComponent.Responsive
+        Hh8VMSdXH={1500}
+        TocVrJHlG={0}
+        VhZFysNo0={"+"}
+        XPtJLY_4J={"Students trained worldwide."}
+        jiFtBNGrU={""}
+      />
+    </div>
+  );
+};
